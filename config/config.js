6715964d7aaf6b83b1f67509a7be8c1445d7222a
Cnwans2 <crystal.nwansi26@gmail.com>
2020-02-13 20:58:21 -0600

    readme

--- config/config.js
+++ config/config.js
@@ -1,13 +1,22 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    username: "root",
+    password: "root",
+    database: "parks_db",
+    host: "localhost",
+    dialect: "mysql"
+  },
+  test: {
+    username: "f2eqb0ai0hjkisot",
+    password: "g21mlsejub645ceu",
+    database: "umblvi0iipa33ins",
+    host: "c584md9egjnm02sk.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    dialect: "mysql"
   },
   production: {
-    use_env_variable: 'JAWSDB_URL',
+    use_env_variable: "JAWSDB_URL",
     dialect: "mysql"
   }
 };
