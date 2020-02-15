230c76c53278ed9c203ad9888bfc7a8daceda93f
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-02-14 22:21:57 -0600

    editing files

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,30 +1,30 @@
-var path = require("path");
+// var path = require("path");
 
-module.exports = function(app) {
-//HTML GET requests for each landing page that user will view
+// module.exports = function(app) {
+// //HTML GET requests for each landing page that user will view
 
-  app.get("/index", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
+//   app.get("/index", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/index.html"));
+//   });
 
-  app.get("/login", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/login.html"));
-  });
+//   app.get("/login", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/login.html"));
+//   });
 
-  app.get("/register", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/register.html"));
-  });
+//   app.get("/register", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/register.html"));
+//   });
 
-  app.get("/search", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/search.html"));
-  });
+//   app.get("/search", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/search.html"));
+//   });
 
-  app.get("/profile", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/profile.html"));
-  });
+//   app.get("/profile", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/profile.html"));
+//   });
 
-  // If no matching route is found default to home
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
-};
+//   // If no matching route is found default to home
+//   app.get("*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/index.html"));
+//   });
+// };
