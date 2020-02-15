3612cd3d066384aea3f61eb41a37b79e7420c6b2
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-02-15 01:03:56 -0600

    login handlers and handlebars finished

---
+++ public/js/login.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    const loginForm = $('form.logs');
+    const emailInp = $('input#inputEmail');
+    const passwordInp = $('input#inputPassword');
+
+    // validating email and password entered
+    loginForm.on('submit', (event) => {
+        console.log('enter listener');
+        event.preventDefault();
+        let userData = {
+            email: emailInp.val().trim(),
+            password: passwordInp.val().trim()
+        };
+
+        if (!userData.email || !userData.password) {
+            return
+        } else {
+            userLogin(userData.email, userData.password)
+            emailInp.val('')
+            passwordInp.val('')
+        }
+    });
+
+    //userLogin posts to "/login" route
+    //if successful redirects to profile page
+    function userLogin(email, password) {
+        console.log('entered user login function');
+        $.post('/login', {
+            email: email,
+            password: password
+        }).then(() => {
+            console.log('password retrieved')
+            window.location.replace('/profile')
+        }).catch ((error) => {
+            console.log(error)
+            console.log('login failed')
+        })
+    }
+});
